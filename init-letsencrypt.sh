42fc276df552a3eb5359aeb4f37c8267d28a82f8
Richard Bruskiewich <richard.bruskiewich@delphinai.com>
2019-03-17 05:43:24 +0100

    Add admin email to system... used for LetsEncrypt SSL too

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -14,7 +14,10 @@ domains=(${hostname})
 rsa_key_size=4096
 data_path="./certbot"
 
-email="" # Adding a valid address is strongly recommended
+# Adding a valid address is strongly recommended
+email=$(egrep -v '^#' .env | grep DC_ADMIN_EMAIL | xargs -0)
+email=${email#*=}
+
 staging=0 # Set to 1 if you're testing your setup to avoid hitting request limits
 
 if [ -d "$data_path" ]; then
